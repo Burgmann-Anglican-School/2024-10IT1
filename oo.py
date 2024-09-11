6a7617dbcc7264a32116b537e787dfed8722922c
Edwin Griffin <8305619+EdwinGriffin@users.noreply.github.com>
2024-09-12 09:28:50 +1000

    Update oo.py

--- oo.py
+++ oo.py
@@ -1,5 +1,8 @@
+#Creating a dog using variables
 dog_name = input("Enter name: ")
 dog_age = int(input("Enter age: "))
 dog_breed = input("Enter breed: ")
+#Loading those variables into a dictionary
 dog = {'name': dog_name, 'age':dog_age,'breed':dog_breed}
+#Printing the dog
 print(f'{dog_name} is a {dog_breed} that is {dog_age} years old.')
