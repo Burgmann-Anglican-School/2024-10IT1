88e43612435a78424f847bf1a8bf4171e56c28f7
Edwin Griffin <8305619+EdwinGriffin@users.noreply.github.com>
2024-09-19 08:44:30 +1000

    Create dog park.py

---
+++ dog park.py
@@ -0,0 +1,42 @@
+class Dog:
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def __str__(self):
+        return f'{self.name} is {self.age} years old'
+    
+    def speak(self, words):
+        return f'{self.name} says {words}'
+
+#This class is a child class of the Dog class.
+class Dachsund(Dog):
+    
+    def speak(self):
+        return f'{self.name} says "oof"'
+
+class Lab(Dog):
+    
+    def speak(self, words='bark'):
+        return super().speak(words)
+
+class Corgi(Dog):
+    def speak(self):
+        return f'{self.name} says "yap"'
+
+dog1 = Dog('Sir Jeffington III', 3)
+dog2 = Dachsund('Sausage', 1)
+dog3 = Lab('Ted', 5)
+dog4 = Corgi('George', 4)
+print('Here are all the dogs:')
+print(dog1)
+print(dog2)
+print(dog3)
+print(dog4)
+
+print('Here is what they say:')
+print(dog1.speak('woof'))
+print(dog2.speak())
+print(dog3.speak('hello'))
+print(dog4.speak())
